608ba4e1c3f78dca5fc4f48f9e0e8eda4056c383
Kelly Burgess <kellyburgess91@gmail.com>
2018-10-19 09:51:26 -0700

    simulate game and determine whether or not player is a winner

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 
-var dieValues=[1, 2, 3, 4, 5, 6];
-
 function rollDice(){
+var dieValues=[1, 2, 3, 4, 5, 6];
 var die1, die2;
 var sum;
-die1=dieValues[Math.random()*dieValues.length];
-die2=dieValues[Math.random()*dieValues.length];
-return sum;
+   
+die1=dieValues[Math.floor(Math.random()*dieValues.length)];
+die2=dieValues[Math.floor(Math.random()*dieValues.length)];
+return die1+die2;
 }
 
-console.log(rollDice());
+function playCraps(){
+var isWinner;
+var firstRoll=rollDice();
+console.log(firstRoll);
+if (firstRoll===7||firstRoll===11){
+    isWinner=true;
 
+}
+else if (firstRoll===2||firstRoll===3||firstRoll===12){
+    isWinner=false;
+}
+else {
+    var point=firstRoll;
+    var newRoll;
+while(newRoll!==point&&newRoll!==7){
+    newRoll=rollDice();
+    console.log(newRoll);
+    if(newRoll===7){
+        isWinner=true;
+    }
+    if(newRoll===point){
+        isWinner=false;
+        
+    }
+}
+}
+console.log(isWinner);
+return isWinner;
+}
+playCraps();
